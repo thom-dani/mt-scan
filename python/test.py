3942770f10b781d9aaa796a144527a48a1bc8138
thomas <thomas.daniel@lip6.fr>
2026-06-14 16:56:02 +0200

    test script

--- python/test.py
+++ python/test.py
@@ -1,46 +1,32 @@
 import mt_scan
 import pickle
 import numpy as np
-from pathlib import Path
-import json
 import time
-import hdbscan
-import argparse
-import os
-import sys
+import matplotlib.pyplot as plt
 
+csv_file = "/workspace/data/dataset-synthetic-1/spherical_5_2.csv"
 
+points = np.loadtxt(csv_file, delimiter=",", skiprows=1)
+points = points[:, :2].astype(np.float32)
 
-#dataset = args.dataset
-pkl_file = f"/worksapce/data/blobs_grid_1000.pkl"
-
-with open(pkl_file, "rb") as f:
-    points = pickle.load(f)
-points = np.array(points)
-
-points = points[:,:2].astype(np.float32)
-
-
-#points = points[:, :2].astype(np.float32).ravel()
 n_points=points.shape[0]
 
 t_start = time.perf_counter()
 
-labels=mt_scan.compute_labels(points, 512, 100)
+labels=mt_scan.compute_labels(points, 512, 25)
 
 t_mtscan = time.perf_counter()
 
 
-print(f"{' -- MTSCAN':<70} | {t_mtscan - t_start:.4f}s", file=sys.stderr)
+print(f"{' -- MTSCAN':<70} | {t_mtscan - t_start:.4f}s")
 
-import matplotlib.pyplot as plt
 plt.figure()
 
 noise = labels == -1
 
 plt.scatter(points[~noise, 0], points[~noise, 1],
-            c=labels[~noise], s=15)
+            c=labels[~noise], s=30)
 plt.scatter(points[noise, 0], points[noise, 1],
             c='gray', s=30, alpha=0.7)
 
-plt.show()
+plt.savefig("/workspace/tmp/test_plot.png", dpi=150, bbox_inches='tight')
