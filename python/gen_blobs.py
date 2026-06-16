9f88ae1f495352cc6f79b9cc3bc1ef7a7ecba8d5
thomas <thomas.daniel@lip6.fr>
2026-06-16 13:22:23 +0200

    blobs gen file

---
+++ python/gen_blobs.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import (
+    make_blobs, make_moons, make_circles, 
+    make_classification, make_s_curve, make_swiss_roll
+)
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+FOLDER = Path(f"../data/time/")
+
+def save_dataset(name, X, y):
+    n_k_points = X.shape[0]//1000
+    data = np.column_stack((X[:, :2], y))
+    filename = Path(FOLDER) / f"{name}_{n_k_points}.csv"
+    np.savetxt(filename, data, delimiter=",", header="x,y,label", comments="")
+
+
+
+n_points_range = range(100_000, 1_000_001, 100_000)
+
+for n in n_points_range:
+
+    save_dataset(
+            "blobs_grid",
+            *make_blobs(n_samples=n, centers=9, n_features=2, cluster_std=0.4, random_state=42)
+        )
+
